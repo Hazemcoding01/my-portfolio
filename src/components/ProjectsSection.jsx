4fb5dc7b1a3caeb992697d36221d77e0b2d756b6
Hazemcoding01 <ethhazem@gmail.com>
2026-08-08 15:29:22 +0300

    Fix project image path cleanly

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import projectImg from '../assets/images/project1.jpeg';
+import projectImg from '../assets/images/mockmate.jpeg';
 
 export default function ProjectsSection() {
   return (
